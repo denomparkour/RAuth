d1e33382151025a5138ee27d725d571191e54df0
denomparkour <denomparkour@gmail.com>
2024-11-07 11:47:38 +0530

    Update: Initial Db Setup

---
+++ RAuth.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RAuth.Core.Models.AddressModel;
+using RAuth.Core.Models.User;
+
+namespace RAuth.Infrastructure.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Address> Address { get; set; }
+    }
+}
