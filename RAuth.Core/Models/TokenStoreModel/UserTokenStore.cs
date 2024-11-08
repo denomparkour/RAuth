4009ef654d3857da0c5c764454ef843865909980
denomparkour <denomparkour@gmail.com>
2024-11-08 16:38:22 +0530

    Update: Implement Refresh Token database

---
+++ RAuth.Core/Models/TokenStoreModel/UserTokenStore.cs
@@ -0,0 +1,18 @@
+﻿using RAuth.Core.Models.User;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RAuth.Core.Models.TokenStoreModel
+{
+    public class UserTokenStore
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public ApplicationUser User { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiryTime { get; set; }
+    }
+}
