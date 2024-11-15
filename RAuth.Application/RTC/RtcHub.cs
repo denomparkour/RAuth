da981de8fdb45f95d2fb1a3b82642e93aa797419
denomparkour <denomparkour@gmail.com>
2024-11-15 16:15:02 +0530

    Update: Include Auth Url as Response

--- RAuth.Application/RTC/RtcHub.cs
+++ RAuth.Application/RTC/RtcHub.cs
@@ -11,9 +11,10 @@ using System.Security.Claims;
 namespace RAuth.Application.RTC
 {
     [Authorize]
-    public class RtcHub(UserManager<ApplicationUser> userManager, IUnitOfWork unitOfWork) : Hub
+    public class RtcHub(UserManager<ApplicationUser> userManager, IUnitOfWork unitOfWork, UserManager<ClientUser> clientUser) : Hub
     {
         private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly UserManager<ClientUser> _clientUser = clientUser;
         private static Dictionary<string, RAuthTransRequest> _pendingRequests = [];
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
@@ -28,19 +29,21 @@ namespace RAuth.Application.RTC
             await base.OnDisconnectedAsync(exception);
         }
 
-        public async Task RequestAccess(string username)
+        public async Task RequestAccess(string accessToken, string username)
         {
             var requesterId = Context.User?.FindFirst(ClaimTypes.Name)?.Value ?? throw new NotFoundException(GlobalConstants.USER_NOT_FOUND);
             var existingUser = await _userManager.FindByNameAsync(username) ?? throw new NotFoundException(GlobalConstants.USER_NOT_FOUND);
+            var clientUser = await _clientUser.FindByIdAsync(requesterId) ?? throw new NotFoundException(GlobalConstants.USER_NOT_FOUND);
             RAuthTransRequest request = new();
             request.RequesterId = requesterId;
             request.ReceiverId = existingUser.Id;
             _pendingRequests[requesterId] = request;
-            await Clients.User(existingUser.Id).SendAsync("ReceiveMessage", "New Authorization Request");
+            await Clients.User(existingUser.Id).SendAsync("ReceiveMessage", clientUser.UserName, accessToken);
         }
 
-        public async Task RespondToRequest(string userId, bool isApproved)
+        public async Task RespondToRequest(string userId, string authUrl, bool isApproved)
         {
+            Console.WriteLine("what values did I get ? " + userId + " " + isApproved);
             var request = _pendingRequests.Values.FirstOrDefault(r => r.ReceiverId == userId) ?? throw new NotFoundException("No Requests Found");
             _pendingRequests.Remove(request.RequesterId);
             if (isApproved)
@@ -48,6 +51,7 @@ namespace RAuth.Application.RTC
                 var user = await _userManager.FindByIdAsync(userId) ?? throw new NotFoundException(GlobalConstants.USER_NOT_FOUND);
                 GetRAuthUserDTO rAuthUserDTO = new();
                 rAuthUserDTO.UserName = user.UserName!;
+                rAuthUserDTO.EncryptedKey = authUrl;
                 GetRAuthUserResponseDTO ResponseData = await _unitOfWork.RAuthRepository.GetRAuthUserAsync(rAuthUserDTO);
                 await Clients.User(request.RequesterId).SendAsync("ReceiveUserData", ResponseData);
             }
